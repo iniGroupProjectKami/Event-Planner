b7d3694455bfc9bd9d973cd65ad2b4b5ea90f9b4
FebrianAditya <febrian.aditya@student.upi.edu>
2020-11-26 17:14:43 +0700

    initiate server

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const cors = require('cors')
+const app = express()
+const PORT = 3000
+const routes = require("./routes/index")
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true })) 
+app.use(routes)
+
+app.listen(PORT, () => {
+    console.log(`i love you ${PORT}`)
+})
